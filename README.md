c16e6a201a4fb50693ae5fc4ef118793c5c2a7a7
Loan Vulliard <lvulliard@cemm.oeaw.ac.at>
2017-10-31 14:01:19 +0100

    Implement minimal UI

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Myeloproliferative neoplasm patient cohort data vizualization
 
-This web application allows the user to explore interactively data from a cohort of 146 myeloproliferative neoplasm blood donors and 11 healthy blood controls.
+This web application allows the user to explore interactively data from a cohort of 113 myeloproliferative neoplasm (MPN) blood donors and 15 healthy blood controls.
 
 ## To-do next
 
--- app.R
+++ app.R
@@ -3,5 +3,27 @@ library(shinyBS)
 library(plotly)
 library(ggplot2)
 
-data = read.table("naseq_cohort_sample_annotation_fschischlik_2017_10.tsv", sep="\t", stringsAsFactors = FALSE)
-head(data)
+
+data = read.table("naseq_cohort_sample_annotation_fschischlik_2017_10.tsv", sep="\t",
+	stringsAsFactors = FALSE, header=T, comment.char="") # Import data file
+
+data = data[data$flag == 1, -1] # Remove null-flagged patients
+
+data$patient = factor(unlist(data.frame(strsplit(data$unique.sample.id, "#"))[1,])) # Extract unique patient ID from unique sample ID
+
+
+# Define client UI
+shinyUi <- navbarPage(title = "MPN cohort data vizualization",
+	# Starting tab
+	tabPanel(title = "What's this?"),
+
+	# Cohort presentation tab
+	tabPanel(title = "Explore the cohort"),
+)
+
+# Define R server
+shinyServer <- function(input, output) {}
+
+# Start Shiny app
+shinyApp(ui = shinyUi, server = shinyServer)
+
